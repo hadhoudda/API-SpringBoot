3094dc81228fba640332ab7cdde18e7715807f91
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-01-28 11:44:52 +0100

    first commit

---
+++ src/test/java/com/houda/apispring/ApispringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houda.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
